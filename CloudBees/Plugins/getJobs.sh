c78990bdad4de8ea80609a23b124bf932b4a4ac2
Pavlo Vovk <pvovk@cloudbees.com>
2020-06-19 15:00:12 +0300

    Add condition if Plugin job not found Add counting the results Add information to help for output formatted file

--- CloudBees/Plugins/getJobs.sh
+++ CloudBees/Plugins/getJobs.sh
@@ -14,14 +14,14 @@ while getopts "s:d:f:o:h:n" opt; do
     ;;
 	n) number="$OPTARG"
 	;;
-    h) echo """The script required 
+    h) echo """The script required
 	1) xpath tool on console
 	2) session connection to CloudBees CD server
 	The Scripts returns the list of job links for flow server
 	-s: Server of ClodBees CD by default viva.beescloud.com
 	-d: Date of of start the search build of plugins
 	-f: file Name with list of plugins in line separated format
-	-o: output file with results
+	-o: output file with results, formatted-<output file name> - formatted file for JIRA table
 	-n: number of build per Plugin
 	"""
 	exit;
@@ -30,14 +30,18 @@ while getopts "s:d:f:o:h:n" opt; do
     ;;
   esac
 done
-last=""
-version="not found"
+count=0
 host="https://${host}/"
 rm "$fileOut" || true
+rm "formatted-${fileOut}" || true
 startDate="${startDate}T00:00:00.000Z"
 echo "Start date is: ${startDate}"
 echo "File with List of Plugins: ${fileIn}"
 while IFS= read -r pluginName || [[ -n "$pluginName" ]]; do
+	last=""
+	jobId=""
+	version="Not Found"
+	((count++))
 	pluginName=${pluginName//[$'\t\r\n']}
     echo "Plugin Name: $pluginName"
 	ectool findObjects job --filters "[{ propertyName => 'projectName', operator => 'equals', operand1 => 'Github Plugins' },{ propertyName => 'procedureName', operator => 'equals', operand1 => 'Build GCP' },{ propertyName => 'outcome', operator => 'equals', operand1 => 'success' },{ propertyName => 'createTime', operator => 'greaterOrEqual', operand1 => '${startDate}' },{ propertyName => 'jobName', operator => 'contains', operand1 => '${pluginName}' },{ propertyName => 'jobName', operator => 'notLike', operand1 => '%preflight%' }]" > "${pluginName}.xml"
@@ -45,10 +49,14 @@ while IFS= read -r pluginName || [[ -n "$pluginName" ]]; do
 	echo "${last}"
 	xpath -q -e "/response/object/job[finish='${last}']" "${pluginName}.xml"
 	jobId=`xpath -q -e "/response/object/job[finish='${last}']/jobId/text()" "${pluginName}.xml"`
-	version=`ectool getProperty --jobId ${jobId} --propertyName version`
+	echo "${jobId}"
+	if [ $jobId != "" ]
+	then
+		version=`ectool getProperty --jobId ${jobId} --propertyName version`
+	fi
 	jobLink="${host}/commander/link/jobDetails/jobs/${jobId}"
 	echo "${jobLink}"
 	echo "${pluginName}, Version: ${version},  JobLink:  $jobLink" >> "$fileOut"
-	echo "|${pluginName} | ${version} | ${jobLink} |" >> "formatted-${fileOut}"
+	echo "|${count}|${pluginName} | ${version} | ${jobLink} |" >> "formatted-${fileOut}"
 done < "${fileIn}"
 
